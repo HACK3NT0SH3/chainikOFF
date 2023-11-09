6eb7474d693eec1856eed4f430099689b420d660
HACK3NT0SH3 <hack3nt0sh3@duck.com>
2023-11-09 16:23:44 +0300

    First commit

---
+++ page 1/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+    },
+    loop: true,
+    keyboard: {
+        enabled: true,
+    }
+
+});
